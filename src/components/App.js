3ce8259ff13d27ca1bd416453d288e6daf118e82
Sankugarud <sanketgarud1998@gmail.com>
2023-10-03 21:49:59 +0530

    submitting code-Tue Oct 03 2023 21:49:57 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
 
-import React from "react";
+ import React, {useState} from 'react'
 import './../styles/App.css';
 import Todo from "./Todo";
 
 const App = () => {
+  
+       
+const [value, setvalue] = useState('');
+const [data, setdata] = useState([]);
+
+
+const handleClick = () => {
+    setdata([...data, value]);
+    setvalue('');
+}
+const handleDelte = (id) => {
+    const newData = data.filter( (item, idx) => idx !== id)
+    setdata(newData);
+}
   return (
-    <div>
-        <Todo/>
+   <div>
+        <p>To-Do List</p>
+        <input type="text" onChange={(e)=>setvalue(e.target.value)}/>
+        <button onClick={handleClick}>Add Todo</button>
+
+        <ul className="todolists">
+            {
+                data.map( (item, index) => (
+                    <li key={index} className="row">
+                        {item}
+                        <button onClick={ () =>handleDelte(index)}> delete</button>
+                    </li>
+                    
+                ))
+            }
+        </ul>
     </div>
+
   )
 }
 
